6f8a37f9c1468ed1af03d8152892ed5e0220db78
Tulio-Fernandes <157730929+Tulio-Fernandes@users.noreply.github.com>
2025-09-03 00:50:37 +0000

    02/09 Atividade Aula 04

---
+++ Aula04/App.js
@@ -0,0 +1,23 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import DetailsScreen from './screens/DetailsScreen';
+import ProfileScreen from './screens/ProfileScreen'; // Importando a nova tela
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Início" component={HomeScreen} />
+        <Stack.Screen name="Detalhes" component={DetailsScreen} />
+        <Stack.Screen
+          name="Perfil"
+          component={ProfileScreen}
+          options={{ title: 'Meu Perfil 👤' }} // título personalizado no cabeçalho
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
